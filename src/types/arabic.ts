3fe7e9010c5efd09662e6af24a925e9c3f8cd122
Jibran <shareefeng4@gmail.com>
2024-12-07 14:40:38 -0500

    verb flow prod version

--- src/types/arabic.ts
+++ src/types/arabic.ts
@@ -26,4 +26,19 @@ export interface Word {
   surah: string;
   ayah: number;
   explanation: string;
+}
+
+export interface Question {
+  type: 'vocabulary' | 'grammar' | 'partsOfSpeech';
+  word: {
+    arabic: string;
+    translation: string;
+    rule: string;
+    surah: string;
+    ayah: number;
+    explanation: string;
+  };
+  question: string;
+  options: string[];
+  correctAnswer: string;
 } 
